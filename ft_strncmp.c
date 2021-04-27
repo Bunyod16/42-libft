25abbb2697990506e2186d232e35d340f29b959b
Bunyodbek Shamsiddinov <bshamsid@u80z04s03.42kl.edu.my>
2021-04-27 19:09:23 +0800

    done with strncmp and header file
    need to figure out makefiles

---
+++ ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/27 13:23:57 by bshamsid          #+#    #+#             */
+/*   Updated: 2021/04/27 18:49:39 by bshamsid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (n && *s1 && (*s1 == *s2))
+	{
+		++s1;
+		++s2;
+		--n;
+	}
+	if (n == 0)
+		return (0);
+	else
+		return (unsigned char)(*s1) - (unsigned char)(*s2);
+}
---
+++ libft.h
@@ -0,0 +1,6 @@
+#ifndef LIBFT_H 
+# define LIBFT_H
+
+unsigned int ft_strncmp(const char *s1, const char *s2, size_t n)
+
+#endif 
