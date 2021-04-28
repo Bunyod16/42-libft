da70b2f03508c676f88de34aabf8b8ef1a2fcf55
Bunyodbek Shamsiddinov <bshamsid@u80z04s03.42kl.edu.my>
2021-04-28 17:02:42 +0800

    changed libft, added atoi, atoi needs work, not complete yet

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,23 @@
 /*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 14:18:35 by bshamsid          #+#    #+#             */
-/*   Updated: 2021/04/28 14:24:22 by bshamsid         ###   ########.fr       */
+/*   Updated: 2021/04/28 17:01:35 by bshamsid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+static int ft_isspace(const char *str)
+{
+	return ((*str >= 8 && *str <= 13) || *str == 32 );
+}
+
 int		ft_atoi(const char *str)
 {
-	
+	while(ft_isspace(str) == 1)
+	{
+		str++;
+	}
+	printf("%c",*str);
+	return(0);
 }
--- libft.h
+++ libft.h
@@ -6,19 +6,18 @@
 /*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 12:50:05 by bshamsid          #+#    #+#             */
-/*   Updated: 2021/04/28 14:16:52 by bshamsid         ###   ########.fr       */
+/*   Updated: 2021/04/28 15:40:44 by bshamsid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-#include <string.h>
-
+# include <string.h>
 unsigned int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
+int				ft_atoi(const char *str);
 #endif
