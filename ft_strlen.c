8f94d4ae95d295d387113837aa29e2cd10a8a302
a <b>
2021-05-05 17:48:47 +0800

    added ft_strlen and changed libft.h

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/28 12:50:05 by bshamsid          #+#    #+#             */
-/*   Updated: 2021/05/05 16:57:52 by bshamsid         ###   ########.fr       */
+/*   Created: 2021/05/05 16:45:33 by bshamsid          #+#    #+#             */
+/*   Updated: 2021/05/05 16:58:52 by bshamsid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <stddef.h>
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
-unsigned int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_isalpha(int c);
-int				ft_isdigit(int c);
-int				ft_isalnum(int c);
-int				ft_isascii(int c);
-int				ft_isprint(int c);
-int				ft_atoi(const char *str);
-size_t				ft_strlen(const char *s);
-#endif
+size_t	ft_strlen(const char *s)
+{
+	size_t count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	return count;
+}
