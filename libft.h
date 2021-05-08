1d5073a81d12e86e5bd0c5498a2b763712fbf399
Bunyod16 <bunyodshams@gmail.com>
2021-05-08 22:38:48 +0800

    made changes

--- libft.h
+++ libft.h
@@ -6,14 +6,14 @@
 /*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 12:50:05 by bshamsid          #+#    #+#             */
-/*   Updated: 2021/05/08 22:29:37 by bunyod16         ###   ########.fr       */
+/*   Updated: 2021/05/08 22:37:30 by bunyod16         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stddef.h>
-unsigned int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
 int				ft_isalnum(int c);
