bf2216445c1fd7badc3bfcd956688e5be7535c6f
Bunyodbek Shamsiddinov <bshamsid@u90z01s01.42kl.edu.my>
2021-05-20 15:35:15 +0800

    finished bonus

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bshamsid <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 00:57:04 by bshamsid          #+#    #+#             */
-/*   Updated: 2021/05/20 02:06:20 by bshamsid         ###   ########.fr       */
+/*   Updated: 2021/05/20 15:25:11 by bshamsid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,25 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *temp;
 	t_list *res;
-	int i;
+	int start;
 
-	if (!lst || !f)
-		return (NULL);
+	start = 1;
 	res = NULL;
-	i = 0;
 	while (lst)
 	{
-		temp = ft_lstnew(f(lst->content));
-		if (!temp)
+		temp = ft_lstnew(lst->content);
+		temp->content = f(temp->content);
+		if (temp == NULL)
 		{
 			ft_lstclear(&res, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&res, temp);
+		if (!start)
+			ft_lstadd_back(&res, temp);
+		else
+			res = temp;
+		start = 0;
 		lst = lst->next;
-		i++;
 	}
 	return (res);
 }
